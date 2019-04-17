a69d30b14228c8f78c5c47d678cce8894687eba0
huiping_guo <huiping.guo@dena.jp>
2019-04-17 14:33:32 +0900

    project init

---
+++ Podfile
@@ -0,0 +1,9 @@
+platform :ios, '10.0'
+
+inhibit_all_warnings!
+use_frameworks!
+
+target 'skin-beauty-sample' do 
+   pod 'GPUImage'
+end
+
---
+++ skin-beauty-sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  skin-beauty-sample
+//
+//  Created by Huiping Guo on 2019/04/17.
+//  Copyright © 2019 Huiping Guo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
